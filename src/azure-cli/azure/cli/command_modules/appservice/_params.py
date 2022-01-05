908c04adca96865571c41e6c18d0c9b1e1efccf0
grumar <grumar@users.noreply.github.com>
2022-01-05 14:19:28 +0100

    Update net_framework_version documentation
    Guidelines for .NET 6.

--- src/azure-cli/azure/cli/command_modules/appservice/_params.py
+++ src/azure-cli/azure/cli/command_modules/appservice/_params.py
@@ -316,7 +316,7 @@ def load_arguments(self, _):
                        help='use 32 bits worker process or not', arg_type=get_three_state_flag(return_label=True))
             c.argument('php_version', help='The version used to run your web app if using PHP, e.g., 5.5, 5.6, 7.0')
             c.argument('python_version', help='The version used to run your web app if using Python, e.g., 2.7, 3.4')
-            c.argument('net_framework_version', help="The version used to run your web app if using .NET Framework, e.g., 'v4.0' for .NET 4.6 and 'v3.0' for .NET 3.5")
+            c.argument('net_framework_version', help="The version used to run your web app. If using .NET Framework, e.g., for .NET 4.6 use 'v4.0' and for .NET 3.5 use 'v3.0'. If using .NET/.NET Core e.g., for .NET6 use 'v6.0' ")
             c.argument('linux_fx_version', help="The runtime stack used for your linux-based webapp, e.g., \"RUBY|2.5.5\", \"NODE|10.14\", \"PHP|7.2\", \"DOTNETCORE|2.1\". See https://aka.ms/linux-stacks for more info.")
             c.argument('windows_fx_version', help="A docker image name used for your windows container web app, e.g., microsoft/nanoserver:ltsc2016")
             if scope == 'functionapp':
